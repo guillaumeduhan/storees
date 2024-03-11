843bd0e86f25a40b224e732122848bad101c9591
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-03-11 12:38:36 +0400

    first commit

---
+++ src/Avatar.tsx
@@ -0,0 +1,61 @@
+function Avatar({
+  avatarUrl,
+  color = "#3b82f6",
+  gradient = "linear-gradient(145deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+  name,
+  small
+}: {
+  avatarUrl?: string,
+  color?: string,
+  gradient?: string,
+  name?: string,
+  small?: boolean,
+}) {
+
+  const getInitials = () => {
+    if (avatarUrl) return;
+    const INITIALS = "AA"
+    if (!name) return INITIALS;
+    const words = name.split(" ");
+    if (!words) return INITIALS;
+    if (words.length === 0) return INITIALS;
+    const initials = words.map((word) => {
+      if (word[0]) return word[0].toUpperCase()
+    });
+    return initials.join("");
+  };
+
+  const getSize = (isGradient = false) => {
+    if (isGradient) return small ? '42px' : '54px'
+    return small ? '36px' : '48px'
+  }
+
+  return (
+    <div style={{
+      background: gradient,
+      padding: '3px',
+      width: getSize(true),
+      height: getSize(true),
+      borderRadius: getSize(true),
+    }}>
+      <div style={{
+        backgroundImage: `url(${avatarUrl})`,
+        border: '3px solid white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        fontWeight: '700',
+        width: getSize(),
+        height: getSize(),
+        borderRadius: getSize(),
+        backgroundColor: color,
+        cursor: 'pointer',
+        backgroundSize: 'cover'
+      }} onClick={() => alert("hello")}>
+        {getInitials()}
+      </div>
+    </div>
+  )
+}
+export default Avatar
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Avatar from './Avatar.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Avatar  name="Guillaume Duhan" avatarUrl="https://substackcdn.com/image/fetch/w_80,h_80,c_fill,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F9762fc33-c14f-4ac2-9a69-34614838d7e5_545x545.png" />
+  </React.StrictMode>,
+)
